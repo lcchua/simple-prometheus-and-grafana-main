02141bed11d77d42cfa57fe020267bb42951d2ff
lcchua <laichwang@gmail.com>
2024-10-15 12:37:39 +0800

    changed tfstate filename in S3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce7-tfstate"
-    key    = "tf-ex-pro-graf-lcchua.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
+    key    = "tf-pg-lcchua.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
     region = "us-east-1"
   }
   required_version = "~> 1.9.0"
