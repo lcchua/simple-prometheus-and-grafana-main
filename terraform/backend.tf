b9d6128c44dce4fa9d6264f6fea56d89760a300f
lcchua <laichwang@gmail.com>
2024-10-15 12:28:51 +0800

    downgraded tf required version

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,5 @@ terraform {
     key    = "tf-ex-pro-graf-lcchua.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
     region = "us-east-1"
   }
-  required_version = "~> 1.9.5"
+  required_version = "~> 1.9.0"
 }
